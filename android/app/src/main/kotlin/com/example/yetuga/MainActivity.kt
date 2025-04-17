0e2c81708137d5f4514aaabd940e6c636f6034de
Medin Sileshi <medin.sileshi.diro@gmail.com>
2025-04-17 14:37:22 +0300

    Add .gitattributes to normalize line endings

--- android/app/src/main/kotlin/com/example/yetuga/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yetuga
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/providers/firebase_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/firebase_service.dart';
-
-final firebaseServiceProvider = Provider<FirebaseService>((ref) {
-  return FirebaseService();
-});
